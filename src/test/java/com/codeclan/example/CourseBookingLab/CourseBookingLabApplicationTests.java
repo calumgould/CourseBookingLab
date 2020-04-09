e7fb382aa134bcceca388e33b1057b869b31233d
calumgould <calumgould26@gmail.com>
2020-04-09 11:44:42 +0100

    model setup

---
+++ src/test/java/com/codeclan/example/CourseBookingLab/CourseBookingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.CourseBookingLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
