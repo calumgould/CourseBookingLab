bd1705913a056d51b1a498d3f6d31b02ed3a4fb7
calumgould <calumgould26@gmail.com>
2020-04-09 13:33:01 +0100

    controllers done

--- src/main/java/com/codeclan/example/CourseBookingLab/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/CourseBookingLab/controllers/CourseController.java
@@ -1,4 +1,41 @@
 package com.codeclan.example.CourseBookingLab.controllers;
 
+import com.codeclan.example.CourseBookingLab.models.Course;
+import com.codeclan.example.CourseBookingLab.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/courses")
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllBooking() {
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity getBooking(@PathVariable Long id) {
+        return new ResponseEntity<>(courseRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> postBooking(@RequestBody Course course) {
+        courseRepository.save(course);
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteBooking(@PathVariable Long id) {
+        courseRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/codeclan/example/CourseBookingLab/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/CourseBookingLab/controllers/CustomerController.java
@@ -1,4 +1,40 @@
 package com.codeclan.example.CourseBookingLab.controllers;
 
+import com.codeclan.example.CourseBookingLab.models.Customer;
+import com.codeclan.example.CourseBookingLab.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/customer")
 public class CustomerController {
+
+        @Autowired
+        CustomerRepository customerRepository;
+
+        @GetMapping
+        public ResponseEntity<List<Customer>> getAllCustomer() {
+            return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+        }
+
+        @GetMapping(value = "/{id}")
+        public ResponseEntity getCustomer(@PathVariable Long id){
+            return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+        }
+
+        @PostMapping
+        public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer){
+            customerRepository.save(customer);
+            return new ResponseEntity<>(customer, HttpStatus.CREATED);
+        }
+
+        @DeleteMapping(value = "/{id}")
+        public ResponseEntity<Long> deleteCustomer(@PathVariable Long id){
+            customerRepository.deleteById(id);
+            return new ResponseEntity<>(id, HttpStatus.OK);
+        }
 }
